5aeca3ffcf24b74061761dddc7a1105a0636e7e5
Michel Everts <everts@unifact.eu>
2020-07-12 22:26:28 +0200

    #4 Adds an optional border instead of the bottom bar, makes both optional, also makes the color configurable

--- region-highlighter/region-highlighter.js
+++ region-highlighter/region-highlighter.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Region Highlighter
 // @namespace    sitethiefs-ns-scripts
-// @version      0.1.1
+// @version      0.1.2
 // @description  Highlights regions
 // @downloadURL  https://github.com/Sitethief/sitethiefs-ns-scripts/raw/master/region-highlighter/region-highlighter.js
 // @author       Sitethief of Vylixan
-// @copyright    GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 // @match        https://www.nationstates.net/*
 // @grant        none
 // ==/UserScript==
@@ -13,10 +12,20 @@
 (function() {
   'use strict';
   
+  let options = {
+    // Highlights these elements
+    border: true,
+    stripe: false,
+    highlight_color: 'red',
+    // removes the junking button
+    junkingRemoved: true,
+  }
+  
   let regionlist = [
     'the_pacific',
     'lazarus',
     'the_east_pacific',
+    //'the_west_pacific',
     'europeia',
     'the_leftist_assembly',
     'the_communist_bloc',
@@ -28,10 +37,22 @@
   ];
   
   document
-    .querySelectorAll('div.deckcard-container > div.deckcard > figure.front > div.deckcard-stripe > div.deckcard-region > a')
-    .forEach(function (el) {
-      if (regionlist.includes(el.getAttribute('href').replace('region=', ''))) {
-        el.parentElement.parentElement.style.backgroundColor = 'red';
+    .querySelectorAll('div.deckcard-container')
+    .forEach(function (container) {
+      let anchor = container.querySelectorAll('div.deckcard > figure.front > div.deckcard-stripe > div.deckcard-region > a');
+      if (anchor[0] && regionlist.includes(anchor[0].getAttribute('href').replace('region=', ''))) {
+        if (options.stripe) {
+          anchor[0].parentElement.parentElement.style.backgroundColor = options.highlight_color;
+        }
+        if (options.border) {
+          container.style.outline = '5px ' + options.highlight_color + 'solid';
+        }
+        if (options.junkingRemoved) {
+          let buttons = container.querySelectorAll('div.deckcard > figure.front > div.deckcard-flag > div.deckcard-info > div.deckcard-info-cardbuttons a.deckcard-junk-button');
+          if (buttons[0]) {
+            buttons[0].remove();
+          }
+        }
       }
     });
 })();
