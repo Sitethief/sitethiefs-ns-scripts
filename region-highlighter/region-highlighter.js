5e6cc2c3ef9a0e1f65aa8d0ed2f0277d210313a6
Michel Everts <everts@unifact.eu>
2020-07-12 22:47:20 +0200

    #2 WIP

--- region-highlighter/region-highlighter.js
+++ region-highlighter/region-highlighter.js
@@ -5,7 +5,6 @@
 // @description  Highlights regions
 // @downloadURL  https://github.com/Sitethief/sitethiefs-ns-scripts/raw/master/region-highlighter/region-highlighter.js
 // @author       Sitethief of Vylixan
-// @copyright    GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 // @match        https://www.nationstates.net/*
 // @grant        none
 // ==/UserScript==
@@ -13,6 +12,17 @@
 (function() {
   'use strict';
   
+<<<<<<< Updated upstream
+=======
+  let options = {
+    // Highlights these elements
+    border: true,
+    stripe: true,
+    // removfes junking button
+    junkingRemoved: true,
+  }
+  
+>>>>>>> Stashed changes
   let regionlist = [
     'the_pacific',
     'lazarus',
@@ -28,10 +38,31 @@
   ];
   
   document
+<<<<<<< Updated upstream
     .querySelectorAll('div.deckcard-container > div.deckcard > figure.front > div.deckcard-stripe > div.deckcard-region > a')
     .forEach(function (el) {
       if (regionlist.includes(el.getAttribute('href').replace('region=', ''))) {
         el.parentElement.parentElement.style.backgroundColor = 'red';
+=======
+    .querySelectorAll('div.deckcard-container')
+    .forEach(function (container) {
+      let anchor = container.querySelectorAll('div.deckcard > figure.front > div.deckcard-stripe > div.deckcard-region > a');
+      if (anchor[0] && regionlist.includes(anchor[0].getAttribute('href').replace('region=', ''))) {
+        if (options.stripe) {
+          anchor[0].parentElement.parentElement.style.backgroundColor = 'red';
+          anchor[0].parentElement.parentElement.style.marginLeft = '1px';
+          anchor[0].parentElement.parentElement.style.width = '95%';
+        }
+        if (options.border) {
+          container.style.outline = '5px red solid';
+        }
+        if (options.junkingRemoved) {
+          let buttons = container.querySelectorAll('div.deckcard > figure.front > div.deckcard-flag > div.deckcard-info > div.deckcard-info-cardbuttons a.deckcard-junk-button');
+          if (buttons[0]) {
+            buttons[0].remove();
+          }
+        }
+>>>>>>> Stashed changes
       }
     });
 })();
