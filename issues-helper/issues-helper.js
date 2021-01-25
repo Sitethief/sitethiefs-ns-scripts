4b70daf2bd2d25eeb7171da1933d1570c6b90e28
Michel Everts <everts@unifact.eu>
2021-01-25 20:16:55 +0100

    #23 Makes the result red or green based on the value

--- issues-helper/issues-helper.js
+++ issues-helper/issues-helper.js
@@ -18,7 +18,7 @@
  * Filter the results of the issue stats, needs NSLinkIssueResultsStats installed, also highlights in the results
  */
 
-(function() {
+(function () {
     'use strict';
     
     let filterConfigs = {
@@ -68,7 +68,7 @@
     const enactDilemma = window.location.href.includes('page=enact_dilemma');
     
     if (locationIsDilemmas && !locationIsShowDilemma) {
-        let body = document.getElementById("loggedin");
+        let body = document.getElementById('loggedin');
         let currentNationKey = body.dataset.nname;
         const issues = document.querySelectorAll('ul.dilemmalist > li');
         if (issues.length) {
@@ -76,63 +76,78 @@
                 const issueLink = issueLi.querySelector('a.dillistnpaper');
                 const issueID = getIssueID(issueLink.href);
                 const link = `http://www.mwq.dds.nl/ns/results/${issueID}.html?nation=${currentNationKey}`;
-                issueLi.innerHTML = issueLi.innerHTML + '<div style="text-align:center"><a href=" ' + link +' " style="text-decoration:underline;color: red;"> Issue Result Stats </a></div>';
+                issueLi.innerHTML = issueLi.innerHTML + '<div style="text-align:center;margin-bottom:10px;"><a href=" ' + link + ' " style="text-decoration:underline;color: red;"> Issue Result Stats </a></div>';
             });
         }
     } else if (!locationIsDilemmas && locationIsShowDilemma) {
-        let body = document.getElementById("loggedin");
+        let body = document.getElementById('loggedin');
         let currentNationKey = body.dataset.nname;
-        const dilemma = document.getElementById("dilemma");
+        const dilemma = document.getElementById('dilemma');
         let issueID = getIssueID(window.location.href);
         if (dilemma) {
             let issueLink = document.createElement('div');
             const link = `http://www.mwq.dds.nl/ns/results/${issueID}.html?nation=${currentNationKey}`;
-            issueLink.innerHTML = '<div style="align:center"><a href=" ' + link +' " style="text-decoration:underline;color: red;"> Issue Result Stats </a></div>';
+            issueLink.innerHTML = '<div style="text-align:center;margin-bottom:10px;"><a href=" ' + link + ' " style="text-decoration:underline;color: red;"> Issue Result Stats </a></div>';
             dilemma.insertBefore(issueLink, dilemma.firstChild);
         }
     } else if (statsPage) {
         const urlParams = new URLSearchParams(window.location.search);
-        const nationKey = urlParams.get('nation')
+        const nationKey = urlParams.get('nation');
         if (nationKey && filterConfigs[nationKey]) {
             const nationConfig = filterConfigs[nationKey];
             if (statsPage) {
                 document.querySelectorAll('table tbody tr td:nth-child(2) div').forEach(function (div) {
                     let found = false;
-                    for(let key in nationConfig) {
+                    let color = 'black';
+                    for (let key in nationConfig) {
                         if (div.innerText.indexOf(nationConfig[key]) !== -1) {
                             found = true;
+                            if (found) {
+                                let mean = div.innerText.substring(
+                                    div.innerText.lastIndexOf("(") + 1,
+                                    div.innerText.lastIndexOf(")")
+                                );
+                                if (mean.indexOf('-') !== -1) {
+                                    color = 'red';
+                                } else if (mean.indexOf('+') !== -1) {
+                                    color = 'green';
+                                }
+                            }
                         }
                     }
                     if (found === false) {
-                        div.style.display = 'none'
+                        div.style.display = 'none';
+                        
+                    } else {
+                        div.style.color = color;
                     }
                 });
                 document.querySelectorAll('table tbody tr td:nth-child(3)').forEach(function (td) {
-                    td.style.display = 'none'
+                    td.style.display = 'none';
                 });
             }
         }
     } else if (enactDilemma) {
-        let body = document.getElementById("loggedin");
-        if (body){
+        let body = document.getElementById('loggedin');
+        if (body) {
             let currentNationKey = body.dataset.nname;
             const nationConfig = filterConfigs[currentNationKey];
             if (filterConfigs[currentNationKey]) {
-                window.scrollTo(0,document.body.scrollHeight);
-                const buttons = document.getElementById("toggleissuedetail");
+                window.scrollTo(0, document.body.scrollHeight);
+                const buttons = document.getElementById('toggleissuedetail');
                 if (buttons && buttons.children.length && buttons.children[1]) {
                     buttons.childNodes[1].click();
                 }
                 document.querySelectorAll('.wceffects .wc-change').forEach(function (effect) {
                     let span = effect.querySelector('.wc1');
                     let found = false;
-                    for(let key in nationConfig) {
+                    for (let key in nationConfig) {
                         if (span.innerText.indexOf(nationConfig[key]) !== -1) {
                             found = true;
                         }
                     }
                     if (found === true) {
-                        effect.style.border = '5px solid red'
+                        effect.style.border = '5px solid red';
                         effect.style.padding = '3px';
                     }
                 });
@@ -145,7 +160,7 @@
         let spliturl = URL.split('/');
         spliturl.forEach(function (urlPart) {
             if (urlPart.includes('dilemma=')) {
-                issueID = urlPart.substring(urlPart.lastIndexOf("=") + 1);
+                issueID = urlPart.substring(urlPart.lastIndexOf('=') + 1);
             }
             
         });
